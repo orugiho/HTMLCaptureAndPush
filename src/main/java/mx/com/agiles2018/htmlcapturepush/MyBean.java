86a796e05ce934a3cec367c1fe6d04070b8977dc
Rodrigo Amador Martinez Pacheco <orugiho@gmail.com>
2018-10-04 12:25:20 -0500

    rebase

---
+++ src/main/java/mx/com/agiles2018/htmlcapturepush/MyBean.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.agiles2018.htmlcapturepush;
+
+import java.util.Date;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import sun.rmi.runtime.NewThreadAction;
+
+/**
+ *
+ * @author orugiho
+ */
+@ManagedBean
+@RequestScoped
+public class MyBean {
+
+    private static final Logger LOG
+            = Logger.getLogger("MyBean");
+    private String html;
+    private String nombre;
+
+    /**
+     * Creates a new instance of MyBean
+     */
+    public MyBean() {
+    }
+
+    public String getDate() {
+        return new Date().toString();
+    }
+
+    public void registraArchivo() {
+        LOG.info("Registro==>" + html);
+        nombre = nombre + new Date().getTime() + ".html";
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Tu archivo se registro con el nombre "
+                        + nombre));
+        nombre = "";
+        html = "";
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+}
